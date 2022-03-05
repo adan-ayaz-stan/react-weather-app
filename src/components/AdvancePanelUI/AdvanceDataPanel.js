e0d0bf0d46da679872c69f0fa7c66b28d238355c
adan-ayaz-stan <97961980+adan-ayaz-stan@users.noreply.github.com>
2022-03-05 18:49:56 +0500

    Fixation , a little bit...

--- src/components/AdvancePanelUI/AdvanceDataPanel.js
+++ src/components/AdvancePanelUI/AdvanceDataPanel.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import classes from "./advanceDataPanel.module.css";
 import CurrentWeatherDetail from "./CurrentWeatherDetail/CurrentWeatherDetail";
 import SevenDayForecast from "./SevenDayForecast/SevenDayForecast";
@@ -9,30 +9,31 @@ function AdvancePanel(props) {
     lat: 72,
     long: 32,
   });
+
   // const [latitude, setLatitude] = useState(72);
   // const [longitude, setLongitude] = useState(32);
 
-  const getLocation = () => {
+  const getLocation = useCallback(() => {
     if (navigator.geolocation) {
       navigator.geolocation.watchPosition(showPosition);
     } else {
       console.log("Geolocation not supported or disabled.");
     }
-  };
-  const showPosition = (position) => {
+  }, []);
+  const showPosition = useCallback((position) => {
     setCoords({
       lat: position.coords.latitude,
       long: position.coords.longitude,
     });
-  };
-  const weatherDataFetcher = async () => {
+  }, []);
+  const weatherDataFetcher = useCallback(async () => {
     const data = await fetch(
       `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.long}&exclude=hourly,minutely&appid=ecf19eb485f5b0d22c414d31ae6a9b14`
     ).then((res) => {
       return res.json();
     });
     setData(data);
-  };
+  }, []);
   // useEffect(() => {
   //   getLocation();
 
@@ -41,9 +42,9 @@ function AdvancePanel(props) {
   useEffect(() => {
     getLocation();
     weatherDataFetcher();
-  });
+    console.log(weatherDataFetcher());
+  }, [weatherDataFetcher, getLocation]);
 
-  console.log("Advance Panel Running!", isData);
   props.onDataSend(isData);
   return (
     <div className={classes.advancedDataPanel}>
